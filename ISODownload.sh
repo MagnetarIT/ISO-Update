cd8a07c3395de522bc59fd256919a0db5c7c1299
Lewis <Lewis@MacBook-Pro.local>
2018-11-11 19:51:15 +0000

    added check if file is present before downloading

--- ISODownload.sh
+++ ISODownload.sh
@@ -15,18 +15,39 @@ fi
 
 # Windows 10 Enterprise
 URL="https://software-download.microsoft.com/download/pr/17134.1.180410-1804.rs4_release_CLIENTENTERPRISEEVAL_OEMRET_x64FRE_en-us.iso"
-echo -e "${GREEN}Downloading Windows 10 enterprise ${NC}"
-curl $URL -o $DIRECTORY/W10_ENT_X64_1804.iso 
+file="$DIRECTORY/W10_ENT_X64_1804.iso"
+if [ -f "$file" ]
+then
+    echo -e "${GREEN}$file found. Download not required! ${NC}"
+else
+    echo -e "${GREEN}Downloading Windows 10 enterprise ${NC}"
+    curl $URL -o $file
+fi
+
 
 # Windows Server 2016
 URL="https://software-download.microsoft.com/download/pr/Windows_Server_2016_Datacenter_EVAL_en-us_14393_refresh.ISO"
-echo -e "${GREEN}Downloading Windows Server 2016 ${NC}"
-curl $URL -o $DIRECTORY/WS2016_Datacenter_EVAL_en-us_14393.iso 
+file="$DIRECTORY/WS2016_Datacenter_EVAL_en-us_14393.iso"
+if [ -f "$file" ]
+then
+	echo -e "${GREEN}$file found. Download not required!${NC}"
+else
+    echo -e "${GREEN}Downloading Windows Server 2016 ${NC}"
+    curl $URL -o $file
+fi
 
 # Windows Server 2012 R2
 URL="http://download.microsoft.com/download/6/2/A/62A76ABB-9990-4EFC-A4FE-C7D698DAEB96/9600.17050.WINBLUE_REFRESH.140317-1640_X64FRE_SERVER_EVAL_EN-US-IR3_SSS_X64FREE_EN-US_DV9.ISO"
-echo -e "${GREEN}Downloading Windows Server 2012R2 ${NC}"
-curl $URL -o $DIRECTORY/WS2012R2.iso 
+file="$DIRECTORY/WS2012R2.iso"
+if [ -f "$file" ]
+then
+	echo -e "${GREEN}$file found. Download not required!${NC}"
+else
+    echo -e "${GREEN}Downloading Windows Server 2012R2 ${NC}"
+    curl $URL -o $file
+fi
+
+
 
 # Windows Server 2019
 # https://support.microsoft.com/en-us/help/4464619/windows-10-update-history
@@ -34,5 +55,11 @@ curl $URL -o $DIRECTORY/WS2012R2.iso
 
 # CentOS 7
 URL="http://mirror.netweaver.uk/centos/7.5.1804/isos/x86_64/CentOS-7-x86_64-DVD-1804.iso"
-echo -e "${GREEN}Downloading CentOS 7 ${NC}"
-curl $URL -o $DIRECTORY/CentOS_7_1804.iso 
+file="$DIRECTORY/CentOS_7_1804.iso"
+if [ -f "$file" ]
+then
+	echo -e "${GREEN}$file found. Download not required!${NC}"
+else
+    echo -e "${GREEN}Downloading CentOS 7 ${NC}"
+    curl $URL -o $file
+fi
